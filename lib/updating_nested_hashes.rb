5a94d8d956295df9cb0c223b86f8fe18bc1c9233
Elena Dinca <elenadinca25@gmail.com>
2020-03-31 13:36:45 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -54,12 +54,12 @@ def add_characters
     }
 
 epic_tragedy[:additional_characters] = []
-prince = { name: "Prince Escalus"}
-apothecary = { name: "Apothecary"}
-epic_tragedy[:additional_characters] << prince
-epic_tragedy[:additional_characters] << apothecary
-#epic_tragedy[:additional_characters][:name] = "Apothecary"
-#epic_tragedy[:additional_characters][:name] = "Apothecary"
+#prince = { name: "Prince Escalus"}
+#apothecary = { name: "Apothecary"}
+#epic_tragedy[:additional_characters] << prince
+#epic_tragedy[:additional_characters] << apothecary
+epic_tragedy[:additional_characters][:name] = "Prince Escalus"
+epic_tragedy[:additional_characters][:name] = "Apothecary"
 
    # Write code below that does the following programmatically:
    #
